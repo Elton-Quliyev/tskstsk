79bf4466c007fae2deaaaba3500b6452413b6db0
Elton-Quliyev <equliyev118@gmail.com>
2024-01-20 14:03:16 +0400

    .

---
+++ main.js
@@ -0,0 +1,38 @@
+// //*2. Tıklandığında seçimlərini göstərən və gizlədən bir açılan menyu yaratmaq üçün JavaScript proqramı yazın.
+
+// const button = document.querySelector(".btn");
+// const list = document.querySelector(".list");
+
+// button.addEventListener("click", function () {
+   
+//   if (list.style.display == "none") {
+//     list.style.display = "block";
+//   } else {
+//     list.style.display = "none";
+//   }
+// });
+
+
+
+//Verilmiş inteqerin 100 və ya 400-ə qədər 20-yə yaxın olub-olmadığını yoxlamaq üçün JavaScript proqramı yazın.
+// function test(x) {
+   
+//     return ((Math.abs(100 - x) <= 20) || (Math.abs(400 - x) <= 20));
+//   }
+  
+//   console.log(test(10));
+
+
+
+
+//Verilmiş müsbət rəqəmin 3 və ya 7 çoxluğu olub-olmadığını yoxlamaq üçün JavaScript proqramı yazın.
+// function test37(x) {
+//     if (x % 3 == 0 || x % 7 == 0) {
+//       return true;
+//     } 
+//     else {
+//       return false;
+//     }
+//   }
+  
+//   console.log(test37(12));
